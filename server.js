fab0305aa52e4df19d2d4f4f0f62fdadc7fe778c
Your Name <olubodekehinde2019@gmail.com>
2024-12-16 12:30:02 +0000

    latest cors link added

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ app.use(
     origin: [
       "https://thrifftify-frontend-njpbrsoxp-10digitminds-projects.vercel.app",
       "http://localhost:3000",
-      "https://thrifftify-backend.onrender.com"
+      "https://thrifftify-backend.onrender.com",
+      'https://thrifftify-fronend.vercel.app'
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
