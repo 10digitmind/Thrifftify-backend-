2559530c2f6b38e703d6c9f8c06e88f930129260
Your Name <olubodekehinde2019@gmail.com>
2024-12-22 00:12:50 +0000

    full url

--- controllers/controller.js
+++ controllers/controller.js
@@ -193,7 +193,7 @@ const loginUser = asyncHandler(async (req, res) => {
     expires: new Date(Date.now() + 1000 * 86400), // 1 day
     sameSite: "lax",
     secure: true,
-    domain:'thrifftify-backend.onrender.com'
+    domain:'https://thrifftify-backend.onrender.com'
     
   });
 
