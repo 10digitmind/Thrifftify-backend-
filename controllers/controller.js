27ef720be6613cf62e69eb6f46dccbbd4b0df926
Your Name <olubodekehinde2019@gmail.com>
2025-04-22 18:20:37 +0100

    latest

--- controllers/controller.js
+++ controllers/controller.js
@@ -1382,7 +1382,7 @@ const Paymentverification = asyncHandler(async (req, res) => {
 
     if (data.status === "success") {
 
-      const metadata = response.data.metadata;
+      const metadata = data.metadata;
       console.log('Verified metadata:', metadata);
       return res.status(200).json({
         message: "Payment verified successfully",
