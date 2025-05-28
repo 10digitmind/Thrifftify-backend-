be8f5654896a2208fea5adf12b3ca8805d180cf5
Your Name <olubodekehinde2019@gmail.com>
2025-05-28 14:32:42 +0100

    coupon code fixed

--- controllers/controller.js
+++ controllers/controller.js
@@ -3100,7 +3100,7 @@ const spin = async (req, res) => {
 
       const randomNumber = Math.floor(1000 + Math.random() * 9000); // Generates a 4-digit random number
 
-        const couponCode = `SPINWIN${randomNumber}`;
+      couponCode = `SPINWIN${randomNumber}`;
 
       await Coupon.create({
         userId,
